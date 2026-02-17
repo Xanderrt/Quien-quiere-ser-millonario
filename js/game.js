9e1c5f6e514056a38d9dbbaf019906a10c377bdd
Alexander Patiño <dgrafico4s@gmail.com>
2026-02-17 10:06:49 -0500

    solucionando rutas

--- js/game.js
+++ js/game.js
@@ -139,7 +139,7 @@ getNewQuestions = () => {
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
     localStorage.setItem("mostRecentScore", score);
 
-    return window.location.assign("end.html");
+    return window.location.href = "./end.html";
   }
 
   questionCounter++;
